ed611de5edc528fa98a3e93bbfb7db301e0c7b37
Guilherme Alberto <guilhermecamelo86@gmail.com>
2022-10-14 14:58:27 -0300

    Final comments

--- src/lab2/RegistroResumos.java
+++ src/lab2/RegistroResumos.java
@@ -1,11 +1,33 @@
 package lab2;
 
+/**
+ * Classe que registra resumos de temas específicos e os gerencia
+ */
 public class RegistroResumos {
+    /**
+     * Array com os temas dos resumos.
+     */
     private String[] temas;
+    /**
+     * Array com o conteúdo de cada resumo
+     */
     private String[] resumos;
+    /**
+     * Indice usado em vários metodos para adicionar e gerenciar os resumos.
+     * Zera após o número de resumos exceder o permitido e começa a substituir os resumos existentes.
+     */
     private int indiceVariavel;
+    /**
+     * Indice usado em vários metodos para adicionar e gerenciar os resumos.
+     * Não zera.
+     */
     private int indiceFinal;
 
+    /**
+     * Constrói um registro de resumos com um número limite específico.
+     * 
+     * @param numeroDeResumos Limite de resumos a serem armazenados
+     */
     public RegistroResumos(int numeroDeResumos) {
         this.temas = new String[numeroDeResumos];
         this.resumos = new String[numeroDeResumos];
@@ -13,6 +35,12 @@ public class RegistroResumos {
         this.indiceFinal = 0;
     }
 
+    /**
+     * Adiciona resumos, recebendo o tema e o conteúdo separadamente
+     * 
+     * @param tema Tema do resumo.
+     * @param resumo Conteúdo do resumo.
+     */
     public void adiciona(String tema, String resumo) {
         if (this.indiceVariavel >= this.resumos.length) {
             this.indiceVariavel = 0;
@@ -28,6 +56,11 @@ public class RegistroResumos {
         }
     }
 
+    /**
+     * Pega todos os resumos já formatados com tema e título e joga em um array.
+     * 
+     * @return Array com todos os resumos já registrados.
+     */
     public String[] pegaResumos() {
         String[] resumosCompletos = new String[this.indiceFinal];
 
@@ -37,6 +70,11 @@ public class RegistroResumos {
         return resumosCompletos;
     }
 
+    /**
+     * Imprime a quantidade de resumos cadastrados e os temas de cada um.
+     * 
+     * @return String com a quantidade de resumos cadastrados e o tema de cada um.
+     */
     public String imprimeResumos() {
         String retorno = "- " + this.indiceFinal + " resumo(s) cadastrado(s)\n" + "-";
         for (int i = 0; i < this.indiceFinal; i++) {
@@ -49,10 +87,21 @@ public class RegistroResumos {
         return retorno;
     }
 
+    /**
+     * Conta a quantidade de resumos cadastrados.
+     * 
+     * @return Quantidade de resumos cadastrados.
+     */
     public int conta() {
         return this.indiceFinal;
     }
 
+    /**
+     * Procura um resumo de um tema específico e retorna um valor booleano dizendo se esse resumo existe ou não.
+     * 
+     * @param tema Tema a ser pesquisado.
+     * @return Boolean dizendo se o resumo foi encontrado ou não.
+     */
     public boolean temResumo(String tema) {
         for (int i = 0; i < this.indiceFinal; i++) {
             if (tema.equals(this.temas[i])) {
